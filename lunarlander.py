7aff9e3e270bdb8d1a1f0c017834ed716c35aa2b
BananaSplit2 <51517972+BananaSplit2@users.noreply.github.com>
2020-01-03 05:54:23 +0100

    Documentation

--- lunarlander.py
+++ lunarlander.py
@@ -6,10 +6,26 @@ from upemtk import *
 from math import *
 from time import sleep
 
+# Constantes
 VITESSE_MAX = 6
 VITESSE_ANG_MAX = 5
+FRAMERATE = 60
+
+def vers_polaire(vecteur):
+    """Convertit coordonnées cartésiennes en coordonnées polaires""" 
+    x, y = vecteur
+    return (hypot(x, y), atan2(y, x))
+    
+def vers_cartes(vecteur):
+    """Convertit coordonnées polaires en coordonnées cartésiennes""" 
+    norme, angle = vecteur
+    return (norme * cos(angle), norme * sin(angle))
 
 def affiche_fusee(position, angle):
+    """Affiche un rectangle représentant la fusée
+    :param position: tuple, représentant la position x, y de la fusée
+    :param angle: float, représentant l'angle actuel de la fusée
+    """
     x0, y0 = position
 
     x1 = cos(radians(-angle) - atan(1/2)) * hypot(40, 20) / 2
@@ -23,22 +39,48 @@ def affiche_fusee(position, angle):
     polygone(poly, remplissage='light blue')
 
 def affiche_sol():
+    """Affiche un sol plat"""
     rectangle(0, 800, 900, 900, remplissage='grey')
 
+def update_propulsion(touche, angle):
+    """Met à jour le vecteur accélération de la propulsion principale de
+    la fusée
+    :param touche: bool, la touche de propulsion est-elle appuyée
+    :param angle: float, angle de la fusée
+    """
+    if touche:
+        return (0.1 * cos(radians(angle))), 0.1 * sin(radians(angle))
+    return (0, 0)
+
 def update_vitesse(fusee, vitesse, gravite, propulsion):
+    """Met à jour le vecteur vitesse de la fusée
+    :param fusee: tuple, position x, y de la fusée
+    :param vitesse: tuple, vecteur vitesse de la fusée
+    :param gravité: tuple, vecteur accélération de la gravité
+    :param propulsion: tuple, vecteur accélération de la propulsion
+    :return: tuple, vecteur vitesse mise à jour
+    """
     vx, vy = vitesse
     ax, ay = gravite
     px, py = propulsion
-    
-    if check_on_ground(fusee, vitesse) and ay + py <= 0:
+
+    # Si la fusée touche le sol, elle s'arrête
+    if check_on_ground(fusee, vitesse):
         return (0, 0)
     
     if hypot(vx + ax + px, vy + ay + py) <= VITESSE_MAX:
         return (vx + ax + px, vy + ay + py)
     else:
-        return vitesse
+        # Si la vitesse maximale est atteinte, on passe par les coordonnées
+        # polaire pour correctement mettre à jour l'angle du vecteur tout en
+        # fixant la norme à la vitesse maximale
+        return vers_cartes((VITESSE_MAX, vers_polaire((vx + ax + px, vy + ay + py))[1]))
 
 def update_acceleration_angulaire(touche):
+    """Met à jour l'accélération angulaire selon la touche pressée
+    :param touche: string, touche du clavier appuyée
+    :return float: accélération angulaire
+    """
     if touche == 'Left':
         return 0.1
     elif touche == 'Right':
@@ -47,12 +89,21 @@ def update_acceleration_angulaire(touche):
         return 0
 
 def update_vitesse_angulaire(vitesse_angulaire, acceleration):
+    """Met à jour la vitesse angulaire
+    :param vitesse_angulaire: float
+    :param acceleration: float, accélération angulaire
+    :return float:"""
+    
+    # Si acceleration en cours, on accelere, sauf si la vitesse angulaire
+    # maximale est déjà atteinte
     if acceleration != 0:
         if fabs(vitesse_angulaire + acceleration) > VITESSE_ANG_MAX:
             return vitesse_angulaire
         else:
             return vitesse_angulaire + acceleration
 
+    # Si pas d'acceleration en cours, on décélère
+    # si la vitesse est déjà assez basse, on arrête le mouvement rotatif
     if fabs(vitesse_angulaire) < 1:
         return 0
     elif vitesse_angulaire < 0:
@@ -61,9 +112,19 @@ def update_vitesse_angulaire(vitesse_angulaire, acceleration):
         return vitesse_angulaire - 0.1
 
 def update_angle(angle, vitesse_angulaire):
+    """Met à jour l'angle de la fusée
+    :param angle: float
+    :param vitesse_angulaire: float
+    :return float:
+    """
     return angle + vitesse_angulaire
 
 def move_fusee(fusee, vitesse):
+    """Met à jour la position de la fusée
+    :param fusee: tuple, position x, y de la fusée
+    :param vitesse: tuple, vecteur vitesse de la fusée
+    :return tuple:
+    """
     x0, y0 = fusee
     vx, vy = vitesse
 
@@ -73,6 +134,7 @@ def move_fusee(fusee, vitesse):
         return (x0 + vx, 800 - 20)
 
 def check_gnd_collision(fusee, vitesse):
+    """Prototype. Vérifie une éventuelle collision avec un sol plat fixe"""
     x0, y0 = fusee
     vx, vy = vitesse
 
@@ -81,17 +143,13 @@ def check_gnd_collision(fusee, vitesse):
     return False
 
 def check_on_ground(fusee, vitesse):
+    """Prototype. Renvoie True si fusée sur le sol"""
     x0, y0 = fusee
     vx, vy = vitesse
     
     if y0 + 21 >= 800:
         return True
     return False
-
-def update_propulsion(touche, angle):
-    if touche:
-        return (0.1 * cos(radians(angle))), 0.1 * sin(radians(angle))
-    return (0, 0)
 
 if __name__ == '__main__':
 
@@ -103,7 +161,7 @@ if __name__ == '__main__':
     fusee_angle = 90        # Angle en degrés de la fusée
     fusee_vit = (0, -1)     # Vecteur vitesse de la fusee (x, y)
     fusee_vit_angulaire = 0 # Vitesse angulaire de la fusée
-    fusee_accel_angulaire = 0
+    fusee_accel_angulaire = 0 # Accélération angulaire de la fusée
     gravite = (0, -0.03)    # Vecteur accélération de la gravité (x, y)
     propulsion = (0, 0)     # Vecteur accélération de la propulsion (x, y)
 
@@ -137,13 +195,13 @@ if __name__ == '__main__':
         else:
             fusee_accel_angulaire = 0
 
-        # Mécaniques
+        # Mécaniques du jeu
         fusee_vit_angulaire = update_vitesse_angulaire(fusee_vit_angulaire, fusee_accel_angulaire)
         fusee_angle = update_angle(fusee_angle, fusee_vit_angulaire)
         fusee_vit = update_vitesse(fusee_pos, fusee_vit, gravite, propulsion)
         fusee_pos = move_fusee(fusee_pos, fusee_vit)
         
-        sleep(1/60)
+        sleep(1/FRAMERATE)
 
     # Fermeture de la fenêtre
     ferme_fenetre()
